15128a2625faaf09dba9970a96c74c85d554bbc3
kgashok <lifebalance@gmail.com>
2019-04-09 10:02:41 +0000

    added bubble.py

---
+++ code/bubble.py
@@ -0,0 +1,9 @@
+from pygorithm import * 
+
+# This will print the code for bubble sort
+#print(sorting.bubble_sort.get_code())
+
+my_list = [12, 4, 2, 14, 3, 7, 5]
+
+# to sort the list
+sorted_list = bubble_sort.sort(my_list)
