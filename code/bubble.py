bdd6d72edbc4b9fbe73fb21100278167a9708cab
Ashok Bakthavathsalam <lifebalance@gmail.com>
2019-04-09 10:09:34 +0000

    fixed bubblesort code

--- code/bubble.py
+++ code/bubble.py
@@ -1,9 +1,10 @@
-from pygorithm import * 
+from pygorithm.sorting import bubble_sort
 
 # This will print the code for bubble sort
-#print(sorting.bubble_sort.get_code())
+print(bubble_sort.get_code())
 
 my_list = [12, 4, 2, 14, 3, 7, 5]
 
 # to sort the list
 sorted_list = bubble_sort.sort(my_list)
+print(sorted_list)
