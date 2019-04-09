88aa7a9d5321ce38852a2429d9465818f10019d8
Ashok Bakthavathsalam <lifebalance@gmail.com>
2019-04-09 10:21:18 +0000

    fixed bubble further

--- code/bubble.py
+++ code/bubble.py
@@ -1,10 +1,12 @@
-from pygorithm.sorting import bubble_sort
+from pygorithm import sorting
 
 # This will print the code for bubble sort
-print(bubble_sort.get_code())
+print(sorting.bubble_sort.get_code())
 
 my_list = [12, 4, 2, 14, 3, 7, 5]
 
 # to sort the list
-sorted_list = bubble_sort.sort(my_list)
+sorted_list = sorting.bubble_sort.sort(my_list)
 print(sorted_list)
+
+# help(sorting)  # does not print all the sorts available
