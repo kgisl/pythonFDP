cebce630fb1d049fc6eeac2dec0adf7641535257
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2020-04-04 03:05:07 +0530

    Update distance.py

--- code/distance.py
+++ code/distance.py
@@ -4,10 +4,11 @@ from math import sqrt
 def distance_between(pointA, pointB):
     """Computes the distance between two Point objects.
 
+    @author kgashok
     @param pointA is a tuple
     @param PointB is a tuple
 
-    @return: distance, in floating point value
+    @return distance between points, in float
     """
     x1, y1 = pointA
     x2, y2 = pointB
