96c814ca1fc0ef7b81d828e90e836d38cebb291b
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-04-04 19:11:44 +0000

    added list based circulate

--- code/circulate.py
+++ code/circulate.py
@@ -20,3 +20,23 @@ def circulate(a, b, c):
 
 a, b, c = input("enter 3 values ").split()
 circulate(a, b, c)
+
+def circulate_list(alist):
+    """circulate 'n' elements in a list
+
+    @author kgashok
+    @param alist contains the 'n' elements
+
+    @return nothing
+    """
+    i = len(alist)
+    while i:
+        first = alist.pop(0)
+        alist.append(first)
+        print(*alist)
+        i -= 1
+
+
+alist = input("enter multiple values ").split()
+circulate_list(alist)
+
