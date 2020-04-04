1101978463e8a1573e035c378ec1973437fb6b2c
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-04-04 17:05:14 +0000

    added circulate for unit2

---
+++ code/circulate.py
@@ -0,0 +1,22 @@
+def circulate(a, b, c):
+  """circulate between three variables
+  
+  @author kgashok
+  @param a is a integer
+  @param b is a integer
+  @param c is a integer
+
+  @return nothing
+  """
+  i = 3
+  while i: 
+    t=a
+    a=b
+    b=c
+    c=t
+    print(a, b , c)
+    i -= 1
+  
+
+a, b, c = input("enter 3 values ").split()
+circulate(a, b, c)
