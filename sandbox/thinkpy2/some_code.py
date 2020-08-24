8351731487815f3c448d11f09a6b127b8130e87a
Peter Evans <peter-evans@users.noreply.github.com>
2020-08-24 17:03:13 +0000

    autopep8 action fixes

--- sandbox/thinkpy2/some_code.py
+++ sandbox/thinkpy2/some_code.py
@@ -11,6 +11,8 @@ In this case, just one function to make sure it works.
 
 
 def test_fun():
-<<<<<<< HEAD
-    '''test function to run and validate if the custom packge installation worked'''
-    print("yup -- this worked!!")
+
+
+<< << << < HEAD
+'''test function to run and validate if the custom packge installation worked'''
+print("yup -- this worked!!")
