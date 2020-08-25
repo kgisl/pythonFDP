b2079ac7237053a3113afa4f4a06289525a92591
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-08-25 10:54:46 +0000

    test_fun2() in some package also tested

---
+++ sandbox/thinkpy2/unit2/some_code2.py
@@ -0,0 +1,17 @@
+
+"""
+From https://bit.ly/stepsPackage
+
+Just an example, but this could be a collection of utility functions, etc.
+
+Here would be documentation of what's in this file
+
+In this case, just one function to make sure it works.
+"""
+
+
+def test_fun2():
+    '''test function which is used to validate if the
+    custom packge installation worked successfully
+    '''
+    print("yup -- this worked!!")
