fcdfc061a3fac99a2d1d13494885d470a75a724d
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-09-13 21:45:49 +0530

    added 5 disk output

--- code/hanoi_simple.py
+++ code/hanoi_simple.py
@@ -1,4 +1,4 @@
-c = 0
+c = 0  # global variable, very very hacky, but need to count steps
 
 
 def moveTower(height, fromPole, toPole, withPole):
@@ -18,3 +18,7 @@ def moveDisk(disk, fp, tp):
 
 tower = ['', 'red  ', 'green', 'blue ']
 moveTower(3, "Start", "Dest", "Middle")
+c = 0 # very very hacky!
+print("5 disks")
+tower = ['', 'A', 'B', 'C', 'D', 'E']
+moveTower(5, "Start", "Dest", "Middle")
