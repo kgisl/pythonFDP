ac4b894888e10d3b02c7baf7f565b0eea162a9b6
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-09-13 03:24:31 +0000

    added simple hanoi simulation

---
+++ code/simplehanoi.py
@@ -0,0 +1,16 @@
+c = 0
+def moveTower(height,fromPole, toPole, withPole):
+    if height == 1:
+        moveDisk(height, fromPole, toPole)
+    else:
+        moveTower(height-1,fromPole,withPole,toPole)
+        moveDisk(height, fromPole,toPole)
+        moveTower(height-1,withPole,toPole,fromPole)
+
+def moveDisk(disk, fp,tp):
+    global c
+    c += 1
+    print("Step", c, ": moving Disk", tower[disk], "from",fp,"to",tp)
+
+tower = ['', 'red  ', 'green', 'blue ']
+moveTower(3, "Start", "Dest", "Middle")
