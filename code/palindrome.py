edd13e64faabc4e750123e1d33c8446ce1aa3087
kgashok <kgashok@users.noreply.github.com>
2021-09-21 23:17:44 +0000

    Adjusted files for PEP-8 compliance

--- code/palindrome.py
+++ code/palindrome.py
@@ -5,42 +5,46 @@ def is_palindrome(aseq):
     @param aseq is a sequence of elements, string or list
     '''
     while len(aseq) > 1:
-        first, *aseq, last = aseq 
-        if first != last: 
+        first, *aseq, last = aseq
+        if first != last:
             break
-    else: 
+    else:
         return True
 
     return False
 
+
 def is_palindrome_rec(aseq):
     '''check for palindrome using unpacking, recursively
-    
+
     @author kgashok
     @param aseq is a sequence of elements, string or list
     '''
 
     # terminal case 1
-    if len(aseq) < 2: 
+    if len(aseq) < 2:
         return True
-    
-    first, *aseq, last = aseq 
-    # terminal case 2 
-    if first != last: 
+
+    first, *aseq, last = aseq
+    # terminal case 2
+    if first != last:
         return False
-    
+
     return is_palindrome_rec(aseq)
 
-def is_palindrome_rec_slice(aseq, first='', last=''): 
+
+def is_palindrome_rec_slice(aseq, first='', last=''):
     '''check for palindrome using slicing, recursively
-    
+
     @author kgashok
     @param aseq is a sequence of elements, string or list
     @param first is a character
     @param last is a character
     '''
 
-    if len(aseq) < 2: return True
-    if first != last: return False
+    if len(aseq) < 2:
+        return True
+    if first != last:
+        return False
 
     return is_palindrome_rec_slice(aseq[1:-1], aseq[0], aseq[-1])
