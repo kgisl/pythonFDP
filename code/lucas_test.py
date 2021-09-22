36a98f2c365a8f41a0847d6bf2524cd54e5e4a1f
kgashok <kgashok@users.noreply.github.com>
2021-09-22 14:46:48 +0000

    Adjusted files for PEP-8 compliance

--- code/lucas_test.py
+++ code/lucas_test.py
@@ -1,12 +1,15 @@
 from lucas import sequence_gen
 
+
 def test_for_three_elements():
     assert sequence_gen(3) == [0, 0, 1]
 
+
 def test_for_five_elements():
-    assert sequence_gen(5) == [0, 0, 1, 1, 2] 
-    
+    assert sequence_gen(5) == [0, 0, 1, 1, 2]
+
+
 def test_for_big_numbers():
     assert sequence_gen(20) == \
-          [0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513]
-          
+        [0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274,
+            504, 927, 1705, 3136, 5768, 10609, 19513]
